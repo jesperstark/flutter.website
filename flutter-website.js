57e02f442784a74eaba877cfcb173276289949b3
Jesper Stark <jesper.stark@softhouse.se>
2018-03-22 13:34:20 +0100

    Change from Koa to express.

--- flutter-website.js
+++ flutter-website.js
@@ -1,8 +1,35 @@
-const Koa = require('koa');
-const app = new Koa();
+import path from 'path';
+import express from 'express';
+import compression from 'compression';
+import request from 'request';
+import hogan from 'hogan.js';
+import logger from 'morgan';
 
-app.use(async ctx => {
-    ctx.body = 'Hello world';
+const app = new express();
+
+app.use(logger('dev'))
+  .use(compression())
+  .set('view engine', 'html')
+  .set('layout', 'flutter-website')
+  .enable('view cache')
+  .engine('html', require('hogan-express'))
+  .set('views', __dirname + '/');
+
+const relative = fp => path.join(__dirname, fp);
+
+app.use('/css', express.static(relative('/node_modules/reveal.js/css')))
+  .use('/lib', express.static(relative('/node_modules/reveal.js/lib')))
+  .use('/js', express.static(relative('/node_modules/reveal.js/js')))
+  .use('/plugin', express.static(relative('/node_modules/reveal.js/plugin')))
+  .use('/md', express.static(relative('/md')))
+  .use('/resources', express.static(relative('/resources')));
+
+app.get('/', (req, res) => {
+  res.render('flutter-website', {
+    location: ''
+  });
 });
 
-app.listen(3000);
+var server = app.listen(8000, function () {
+  console.log('Server started: http://localhost:%s/', server.address().port);
+});
---
+++ md/flutter-website.md
@@ -0,0 +1,8 @@
+<!-- .slide: data-background="resources/index.jpg" -->
+
+# [flutter.website](https://flutter.website)
+Presentation of [flutter](https://flutter.io/) in Google Kubernetes Engine [(GKE)](https://cloud.google.com/kubernetes-engine/)
+
+[github.com/jesperstark/flutter.website](https://github.com/jesperstark/flutter.website)
+
+Last update: xx
